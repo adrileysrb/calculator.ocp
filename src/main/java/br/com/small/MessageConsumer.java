5a3a6e85eb2a6a286539406353862a8a2bbe4db0
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2025-07-13 19:14:41 -0300

    another stuff

--- src/main/java/br/com/small/MessageConsumer.java
+++ src/main/java/br/com/small/MessageConsumer.java
@@ -1,15 +1,30 @@
-package br.com.small;
+    package br.com.small;
 
-import org.eclipse.microprofile.reactive.messaging.Incoming;
+    import org.bson.Document;
+    import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-import jakarta.enterprise.context.ApplicationScoped;
+    import com.mongodb.client.MongoClient;
+    import com.mongodb.client.MongoCollection;
 
-@ApplicationScoped
-public class MessageConsumer {
+    import jakarta.enterprise.context.ApplicationScoped;
+    import jakarta.inject.Inject;
 
-    @Incoming("my-incoming-stream")
-    public void consume(String message) {
-        System.out.println(">> Mensagem recebida do RabbitMQ: " + message);
+    @ApplicationScoped
+    public class MessageConsumer {
+
+        @Inject
+        MongoClient mongoClient;
+        
+        @Incoming("set_data_queue")
+        public void consume(String message) {
+            System.out.println(">> Mensagem recebida do RabbitMQ: " + message);
+            Document document = new Document().append("value", message);
+
+            getCollection().insertOne(document);
+        }
+
+        private MongoCollection<Document> getCollection() {
+            return mongoClient.getDatabase("holderdatabase").getCollection("messages");
+        }
+        
     }
-    
-}
